6dfb3d01e0bb001c50b6155cc308e862ff478725
Oleg Kaliadka <oleg92sinister@mail.ru>
2021-08-29 13:32:13 +0200

    first try

--- conf/settings.py
+++ conf/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Job',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
